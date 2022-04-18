04f37922d0a91aeba88922d380413d639bf95215
aymen10095 <aymenmaskhi@gmail.com>
2022-04-18 19:01:12 +0200

    Updates

---
+++ precache-manifest.3fd02b580dfcd657303c7658e589d03a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2a44674d4fbbd1a7eb6",
+    "url": "/chillin/static/css/main.1f3ff2c7.chunk.css"
+  },
+  {
+    "revision": "b2a44674d4fbbd1a7eb6",
+    "url": "/chillin/static/js/main.b2a44674.chunk.js"
+  },
+  {
+    "revision": "6d4d32b2a2439d9222a0",
+    "url": "/chillin/static/css/1.85bc138a.chunk.css"
+  },
+  {
+    "revision": "6d4d32b2a2439d9222a0",
+    "url": "/chillin/static/js/1.6d4d32b2.chunk.js"
+  },
+  {
+    "revision": "e08f5e5ee56bce0d42fd",
+    "url": "/chillin/static/js/runtime~main.e08f5e5e.js"
+  },
+  {
+    "revision": "464c057171c50fc13fc68ac420098280",
+    "url": "/chillin/static/media/avatar0.464c0571.jpg"
+  },
+  {
+    "revision": "d73a92e69542e1b1123cec47496d0085",
+    "url": "/chillin/static/media/logo.d73a92e6.png"
+  },
+  {
+    "revision": "578ef95d9283a290c6cdb0b513b09bfa",
+    "url": "/chillin/static/media/avatar1.578ef95d.png"
+  },
+  {
+    "revision": "89c08b99cc01643e9d4e28ccb71f1c75",
+    "url": "/chillin/static/media/avatar2.89c08b99.png"
+  },
+  {
+    "revision": "4cd459850e97efe8c24c83af8b360ae9",
+    "url": "/chillin/static/media/avatar3.4cd45985.png"
+  },
+  {
+    "revision": "4c703bde003bfbe0d8b317ac9ff57db3",
+    "url": "/chillin/static/media/avatar4.4c703bde.png"
+  },
+  {
+    "revision": "125c9520fa011778fc1d7bf0dbf4d93c",
+    "url": "/chillin/static/media/avatar5.125c9520.png"
+  },
+  {
+    "revision": "83bf2e01a873667fef3ff2c85c038d34",
+    "url": "/chillin/static/media/avatar6.83bf2e01.png"
+  },
+  {
+    "revision": "c7cfa16c061f9851cd658c855682d2f9",
+    "url": "/chillin/static/media/avatar7.c7cfa16c.png"
+  },
+  {
+    "revision": "2165110724747fca4fc19d4fa415cfb9",
+    "url": "/chillin/static/media/avatar8.21651107.png"
+  },
+  {
+    "revision": "a8964fb3bdaccb5d3e6372aee7bca182",
+    "url": "/chillin/static/media/avatar9.a8964fb3.png"
+  },
+  {
+    "revision": "5508dac7bef408d6bd1e9ff8cfaf1aff",
+    "url": "/chillin/static/media/avatar10.5508dac7.png"
+  },
+  {
+    "revision": "9c0fa19321c5f44c91457b6e6791bff9",
+    "url": "/chillin/static/media/avatar11.9c0fa193.png"
+  },
+  {
+    "revision": "d0508a73bd27570bf11d2ac1c660c47a",
+    "url": "/chillin/static/media/avatar12.d0508a73.png"
+  },
+  {
+    "revision": "bc6655d8cf1ba0de1e6e50f148f932d5",
+    "url": "/chillin/static/media/logo2.bc6655d8.png"
+  },
+  {
+    "revision": "565902df542fce75993ccb295af3cdd3",
+    "url": "/chillin/static/media/background.565902df.jpg"
+  },
+  {
+    "revision": "6d4c00d6f7636385dd03d2bed7808b4a",
+    "url": "/chillin/static/media/early_morning.6d4c00d6.png"
+  },
+  {
+    "revision": "b490cb09abfbbc7917614fa114f15ff7",
+    "url": "/chillin/static/media/night.b490cb09.png"
+  },
+  {
+    "revision": "6c387cae0ebd69266e80b515919047fb",
+    "url": "/chillin/static/media/morning.6c387cae.png"
+  },
+  {
+    "revision": "27c59dffd29eee2bdb3b5b0359702f34",
+    "url": "/chillin/static/media/sun_set.27c59dff.png"
+  },
+  {
+    "revision": "fce2158b98cc4fb73e50c43dde8e9499",
+    "url": "/chillin/static/media/early_night.fce2158b.png"
+  },
+  {
+    "revision": "8592081a48d6350951e43149f4001c91",
+    "url": "/chillin/static/media/evening.8592081a.png"
+  },
+  {
+    "revision": "1ac135d52f8c8b52a7fb83489f5b0813",
+    "url": "/chillin/index.html"
+  }
+];
